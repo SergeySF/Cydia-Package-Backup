b912c843b6eca1162b8dd0757257d729f636c05d
Evin Ugur <evinoog96@gmail.com>
2012-10-22 18:13:12 -0400

    Made Package class sensible
    I never knew HashTables sigh...

--- src/Package.java
+++ src/Package.java
@@ -1,52 +1,19 @@
+import java.util.Hashtable;
 public class Package {
-	private String 
-		Package,
-		Essential,
-		Status,
-		Priority,
-		Section,
-		InstalledSize,
-		Maintainer,
-		Architecture,
-		Version,
-		Depends,
-		PreDepends,
-		Description,
-		Name,
-		Homepage;
-	public Package(String Package, String Essential, String Status, String Priority, String Section, String InstalledSize, String Maintainer, String Architecture, 
-			String Version, String Depends, String PreDepends, String Description, String Name, String Homepage){
-		this.Package = Package;
-		this.Essential = Essential;
-		this.Status = Status;
-		this.Priority = Priority;
-		this.Section = Section;
-		this.InstalledSize = InstalledSize;
-		this.Maintainer = Maintainer;
-		this.Architecture = Architecture;
-		this.Version = Version;
-		this.Depends = Depends;
-		this.PreDepends = PreDepends;
-		this.Description = Description;
-		this.Name = Name;
-		this.Homepage = Homepage;
+	private Hashtable<String, String> flags;
+	public Package(){
+		flags = new Hashtable<String, String>();
 	}
-	public String getPackage() {return Package;}
-	public String getEssential() {return Essential;}
-	public String getStatus() {return Status;}
-	public String getPriority() {return Priority;}
-	public String getSection() {return Section;}
-	public String getInstalledSize() {return InstalledSize;}
-	public String getMaintainer() {return Maintainer;}
-	public String getArchitecture() {return Architecture;}
-	public String getVersion() {return Version;}
-	public String getDepends() {return Depends;}
-	public String getPreDepends() {return PreDepends;}
-	public String getDescription() {return Description;}
-	public String getName() {return Name;}
-	public String getHomepage() {return Homepage;}
-	
-	public static String[] FIELDS = {
+	public void add(String key, String value){
+		flags.put(key, value);
+	}
+	public boolean doesHaveKey(String key){
+		return flags.containsKey(key);
+	}
+	public boolean keyHasValue(String key, String value){
+		return doesHaveKey(key) && flags.get(key).equals(value);
+	}
+	public static String[] DEFAULT_FIELDS = {
 		"Package", "Essential", "Status", "Priority",
 		"Section", "InstalledSize", "Maintainer",
 		"Architecture", "Version", "Depends",
